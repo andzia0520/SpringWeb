223973ae013612c9bf3d7283765d000e2465436c
andzia0520 <ankadaniluk@gmail.com>
2020-12-20 23:43:36 +0100

    modul example

---
+++ src/main/java/com/crud/tasks/domain/TrelloCard.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloCard {
+    private String name;
+    private String description;
+    private String pos;
+    private String listId;
+}
