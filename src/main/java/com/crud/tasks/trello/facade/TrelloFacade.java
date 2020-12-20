03db549dce2e0ff38a29c5e9075dab1d851a25bf
andzia0520 <ankadaniluk@gmail.com>
2020-12-20 21:14:43 +0100

    in progress

---
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -0,0 +1,25 @@
+package com.crud.tasks.trello.facade;
+
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.mapper.TrelloMapper;
+import com.crud.tasks.service.TrelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class TrelloFacade {
+
+    @Autowired
+    private TrelloService trelloService;
+
+    @Autowired
+    private TrelloMapper trelloMapper;
+
+    public List<TrelloBoard> fetchTrelloBoards() {
+        return trelloMapper.mapToBoards(trelloService.fetchTrelloBoards());
+    }
+}
